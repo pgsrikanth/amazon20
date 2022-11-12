0c477f88ee9a2955b8427258528beb0dcab6d18f
srikanth <gowtham@1996.com>
2022-11-12 15:14:39 +0530

    amazon 20 login page done by dev1

---
+++ src/test/java/amazon20/AppTest.java
@@ -0,0 +1,20 @@
+package amazon20;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
